af6337a2243416f8b3cad378f45e30c74db395f5
Anushka Gupta <coderstacey0112@gmail.com>
2024-08-06 17:31:14 +0530

    socket io updated link for deployment

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://chat-app-yt.onrender.com", {
+			const socket = io("https://chit-chat-mern-1b8l.onrender.com/", {
 				query: {
 					userId: authUser._id,
 				},
